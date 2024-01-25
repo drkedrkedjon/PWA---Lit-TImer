547c782507eda993e15c9cc772121233289234d8
Sasa Savic Perusina <sasa@bubulazy.com>
2024-01-25 13:45:10 +0000

    Stuck on importing timer component from NPM package

---
+++ app/src/js/app.js
@@ -0,0 +1,14 @@
+import { LitElement, css, html } from "lit";
+import { TimerComponent } from "../../node_modules/@drkedrkedjon/timer/dist/index.js";
+
+export class App extends LitElement {
+  connectedCallback() {
+    super.connectedCallback();
+    window.customElements.define("timer-component", TimerComponent);
+  }
+
+  render() {
+    return html` <h1>Mi APP!</h1>
+      <timer-component></timer-component>`;
+  }
+}
