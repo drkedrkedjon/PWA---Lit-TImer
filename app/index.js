08f1101985f27a96248a50ecbf84b88f73f029a1
Sasa Savic Perusina <sasa@bubulazy.com>
2024-01-26 11:30:50 +0000

    SCSS compiled, app component estiled

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 // export * from "./src/js/app.js";
+import "./src/scss/guide.scss";
+import "./src/scss/first.scss";
+import "./src/scss/components.scss";
 
 import { App } from "./src/js/app.js";
 
 customElements.define("my-app", App);
-document.getElementById("app").innerHTML = "<my-app></my-app>";
+document.getElementById("app").innerHTML = `
+  <my-app></my-app>
+  `;
